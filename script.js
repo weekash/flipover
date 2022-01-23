110590e54274144adbf22b3ae8cb660caf0f4968
vikash chandra <vikash28200@gmail.com>
2022-01-23 20:36:48 +0530

    added logic of game complete and timer

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const board = document.getElementsByClassName('board')[0];
 const initBtn = document.getElementsByClassName('init-btn')[0];
 const counter = document.getElementsByClassName('counter')[0];
 const restart = document.getElementsByClassName('restart');
+const timerDiv = document.getElementsByClassName('timer')[0];
+const endLine = document.getElementsByClassName('endline')[0];
 const scoreBoard = document.getElementById('score');
 const gameover = document.getElementById('gameover');
 const gridFragment = document.createDocumentFragment();
-const COUNTER_TIME = 3000;
+const COUNTER_TIME = 8000;
 const gridSize = 36;
 const uniqueCharCount = gridSize/2;
 const chars1 = [...Array(uniqueCharCount).keys()]
@@ -19,8 +21,12 @@ let firstElement = elements[selection[0]];
 let secondElement = elements[selection[1]];
 let selLen = selection.length;
 let score = 0;
+let time = 0;
+let timerInterval = null;
+
 
 initBtn.addEventListener('click', (e)=>{
+    console.log('uo')
     init();
     e.target.style.display = 'none'
 })
@@ -32,17 +38,20 @@ for(let i=0;i<restart.length;i++){
 }
 
 function startCounter() {
-    let time = 1;
-    const endTime = COUNTER_TIME/1000;
-    counter.style.display = 'flex';
-    counter.textContent = time;
+    let time = COUNTER_TIME/1000;
+    let endTime = 1;
+    counter.style.display = 'block';
+    text = `your game starts in ${time} seconds`
+    counter.textContent = text;
+
     let interval = setInterval(()=>{
         if(time==endTime) {
             clearInterval(interval)
             counter.style.display = 'none'
         }
-        time++;
-        counter.textContent = time;
+        time--;
+        text = `your game starts in ${time} seconds`
+        counter.textContent = text;
     }, 1000)
 }
 
@@ -53,23 +62,39 @@ function setScore(number) {
 
 function checkGameOver() {
     if(score === gridSize) {
+        endLine.textContent = `Congrats. Well played, you won this round in ${time} seconds`;
         gameover.style.display='block';
+        clearInterval(timerInterval);
+        board.innerHTML = '';
+
     }
 }
 
-function init(){
+function boxContent(value){
+    return `
+    <div class="content">
+            <div class="front">
+            </div>
+            <div class="back">
+                ${value}
+            </div>
+        </div>
+    `
+}
+ function init(){
     for(let i=0;i<gridSize;i++) {
         const box = document.createElement('div')
         box.className = 'box';
-        box.textContent = alpha[i]
+        box.dataset.value = alpha[i];
         box.dataset.id = i;
+        box.innerHTML = boxContent(alpha[i]);
         elements.push(box)
         gridFragment.appendChild(box)
     }
     board.appendChild(gridFragment)
     scoreBoard.style.display = 'block'
     startCounter();
-    setTimeout(()=>flipAll(elements), COUNTER_TIME);
+    setTimeout(()=>{flipAll(elements); addListener()}, COUNTER_TIME);
 }
 function shuffle(chars){
     for(let i=0;i<chars.length;i++) {
@@ -96,8 +121,8 @@ function emptySelection(){
     selLen = 0;
 }
 function checkSame(firstElement, secondElement){
-    let first = firstElement.textContent;
-    let second = secondElement.textContent
+    let first = firstElement.dataset.value;
+    let second = secondElement.dataset.value
     if(first && second && first === second){
         setTimeout(()=>{
             firstElement.style.visibility = 'hidden'
@@ -109,27 +134,38 @@ function checkSame(firstElement, secondElement){
     emptySelection();
     }
 }
-board.addEventListener('click',(e)=>{
-    let currentElement = e.target;
-    let dataId = currentElement.dataset.id;
-    if(!currentElement.classList.contains('box'))
-    return;
-    if(selLen==2){
-        flipAll([firstElement, secondElement]);
-       emptySelection();
-    }
-    if(selection.includes(dataId)){
-        flipOne(firstElement);
-       emptySelection();
+
+function addListener() {
+    board.addEventListener('click',(e)=>{
+        let currentElement = e.target.offsetParent.offsetParent;
+        let dataId = currentElement.dataset.id;
+        if(!currentElement.classList.contains('box'))
         return;
-    }
-    currentElement.classList.remove('hidden')
-    selection.push(dataId);
-    selLen = selection.length;
-    setElements();
-    if(selLen == 2){
-        checkSame(firstElement, secondElement)
-    }
-})
-
-
+        if(selLen==2){
+            flipAll([firstElement, secondElement]);
+        emptySelection();
+        }
+        if(selection.includes(dataId)){
+            flipOne(firstElement);
+        emptySelection();
+            return;
+        }
+        currentElement.classList.remove('hidden')
+        selection.push(dataId);
+        selLen = selection.length;
+        setElements();
+        if(selLen == 2){
+            checkSame(firstElement, secondElement)
+        }
+    })
+    timerDiv.style.display = 'block'
+    clearInterval(timerInterval);
+    timerInterval = setInterval(()=>{
+        time+=1
+        timerDiv.textContent = time;
+        if(time > 500) {
+            clearInterval(timerInterval);
+            location.reload();
+        } 
+    },1000)
+}
