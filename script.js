e7746ee4908377c6dcadb64ccd21d6694c8c9d13
vikash chandra <vikash28200@gmail.com>
2022-02-22 12:34:53 +0530

    fixed one of the issue while swapping cards

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ const gameover = document.getElementById('gameover');
 const gridFragment = document.createDocumentFragment();
 const COUNTER_TIME = 8000;
 const gridSize = 36;
-const uniqueCharCount = gridSize/2;
+const uniqueCharCount = gridSize / 2;
 const chars1 = [...Array(uniqueCharCount).keys()]
 const chars2 = [...Array(uniqueCharCount).keys()]
 const chars = [...chars1, ...chars2]
 const elements = [];
 shuffle(chars)
-const alpha = chars.map((char)=> String.fromCharCode(char + 65))
+const alpha = chars.map((char) => String.fromCharCode(char + 65))
 let selection = [];
 let firstElement = elements[selection[0]];
 let secondElement = elements[selection[1]];
@@ -25,26 +25,26 @@ let time = 0;
 let timerInterval = null;
 
 
-initBtn.addEventListener('click', (e)=>{
+initBtn.addEventListener('click', (e) => {
     init();
     e.target.style.display = 'none'
 })
 
-for(let i=0;i<restart.length;i++){
-    restart[i].addEventListener('click', (e)=>{
-    location.reload();
+for (let i = 0; i < restart.length; i++) {
+    restart[i].addEventListener('click', (e) => {
+        location.reload();
     })
 }
 
 function startCounter() {
-    let time = COUNTER_TIME/1000;
+    let time = COUNTER_TIME / 1000;
     let endTime = 1;
     counter.style.display = 'block';
     text = `your game starts in ${time} seconds`
     counter.textContent = text;
 
-    let interval = setInterval(()=>{
-        if(time==endTime) {
+    let interval = setInterval(() => {
+        if (time == endTime) {
             clearInterval(interval)
             counter.style.display = 'none'
         }
@@ -55,21 +55,21 @@ function startCounter() {
 }
 
 function setScore(number) {
-    scoreBoard.textContent =  number;
+    scoreBoard.textContent = number;
     checkGameOver();
 }
 
 function checkGameOver() {
-    if(score === gridSize) {
+    if (score === gridSize) {
         endLine.textContent = `Congrats. Well played, you won this round in ${time} seconds`;
-        gameover.style.display='block';
+        gameover.style.display = 'block';
         clearInterval(timerInterval);
         board.innerHTML = '';
 
     }
 }
 
-function boxContent(value){
+function boxContent(value) {
     return `
     <div class="content">
             <div class="front">
@@ -80,8 +80,8 @@ function boxContent(value){
         </div>
     `
 }
- function init(){
-    for(let i=0;i<gridSize;i++) {
+function init() {
+    for (let i = 0; i < gridSize; i++) {
         const box = document.createElement('div')
         box.className = 'box';
         box.dataset.value = alpha[i];
@@ -93,11 +93,11 @@ function boxContent(value){
     board.appendChild(gridFragment)
     scoreBoard.style.display = 'block'
     startCounter();
-    setTimeout(()=>{flipAll(elements); addListener()}, COUNTER_TIME);
+    setTimeout(() => { flipAll(elements); addListener() }, COUNTER_TIME);
 }
-function shuffle(chars){
-    for(let i=0;i<chars.length;i++) {
-        let newIndex = Math.floor(Math.random()*(chars.length));
+function shuffle(chars) {
+    for (let i = 0; i < chars.length; i++) {
+        let newIndex = Math.floor(Math.random() * (chars.length));
         [chars[i], chars[newIndex]] = [chars[newIndex], chars[i]];
     }
 }
@@ -105,70 +105,72 @@ function flipOne(item) {
     item.classList.add('hidden')
 }
 function flipAll(items) {
-    items.forEach((item)=>{
+    items.forEach((item) => {
         item.classList.add('hidden')
     })
 }
-function setElements(){
+function setElements() {
     firstElement = elements[selection[0]]
     secondElement = elements[selection[1]]
 }
 
-function emptySelection(){
-    selection=[];
+function emptySelection() {
+    selection = [];
     setElements();
     selLen = 0;
 }
-function checkSame(firstElement, secondElement){
+function checkSame(firstElement, secondElement) {
     let first = firstElement.dataset.value;
     let second = secondElement.dataset.value
-    if(first && second && first === second){
-        setTimeout(()=>{
+    if (first && second && first === second) {
+        setTimeout(() => {
             // firstElement.style.visibility = 'hidden'
             // secondElement.style.visibility = 'hidden'
             firstElement.classList.add('removed')
             secondElement.classList.add('removed')
-            score +=2
+            score += 2
             setScore(score)
 
-        },300)
-    emptySelection();
+        }, 300)
+        emptySelection();
     }
 }
-function startTimer(){
+function startTimer() {
     timerDiv.style.display = 'block'
     clearInterval(timerInterval);
-    timerInterval = setInterval(()=>{
-        time+=1
+    timerInterval = setInterval(() => {
+        time += 1
         timerDiv.textContent = time;
-        if(time > 500) {
+        if (time > 500) {
             clearInterval(timerInterval);
             location.reload();
-        } 
-    },1000)
+        }
+    }, 1000)
 }
 function addListener() {
-    board.addEventListener('click',(e)=>{
+    board.addEventListener('click', (e) => {
         let currentElement = e.target.offsetParent.offsetParent;
         let dataId = currentElement.dataset.id;
-        if(!currentElement.classList.contains('box'))
-        return;
-        if(selLen==2){
-            flipAll([firstElement, secondElement]);
-        emptySelection();
-        }
-        if(selection.includes(dataId)){
-            flipOne(firstElement);
-        emptySelection();
+        if (!currentElement.classList.contains('box'))
             return;
+        if (selection.includes(dataId)) {
+            flipOne(elements[dataId]);
+            selection.splice(selection.indexOf(dataId), 1)
+            selLen = selection.length;
+            return;
+        }
+        if (selLen == 2) {
+            flipAll([firstElement, secondElement]);
+            emptySelection();
         }
         currentElement.classList.remove('hidden')
         selection.push(dataId);
         selLen = selection.length;
         setElements();
-        if(selLen == 2){
+        console.log(selection)
+        if (selLen == 2) {
             checkSame(firstElement, secondElement)
         }
     })
-   startTimer();
+    startTimer();
 }
